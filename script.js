5a1121e8b3f30d423ed6b41f4773e946f77ed6ff
Nazimmulani786 <113841284+Nazimmulani786@users.noreply.github.com>
2023-08-14 15:34:45 +0530

    Level 1 Task 2
    Level 1 Task 2

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
-'use strict';
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-/**
- * add event on element
- */
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer...!", "coder...!","Designer...!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer...!", "Coder...!", "Designer...!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-
-/**
- * navbar toggle
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
-
-const toggleNavbar = function () { navbar.classList.toggle("active"); }
-
-addEventOnElem(navTogglers, "click", toggleNavbar);
-
-const closeNavbar = function () { navbar.classList.remove("active"); }
-
-addEventOnElem(navLinks, "click", closeNavbar);
-
-
-
-/**
- * header & back top btn active
- */
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
